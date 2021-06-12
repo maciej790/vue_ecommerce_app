4b76d632d998dab1a05ac2bfce1fb073bb1a12ff
maciej790 <maciejmw126@gmail.com>
2021-06-12 22:14:41 +0200

    done navigate

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <TopNavigate />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import TopNavigate from './components/TopNavigate.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    TopNavigate,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./style/App.scss";
 </style>
---
+++ src/components/TopNavigate.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="navigate">
+    <div class="navigate__logo">
+      <i class="fas fa-store-alt fa-2x"></i>
+    </div>
+    <div class="navigate__button" @click="isClicked = !isClicked">
+      <i class="fas fa-ellipsis-h fa-2x"></i>
+    </div>
+    <ul :class="isClicked ? 'open' : 'navigate__options'">
+      <li
+        class="navigate__options--item"
+        v-for="(item, index) in menuItems"
+        :key="index"
+        @click="isClicked = !isClicked"
+      >
+        {{ item }}
+      </li>
+    </ul>
+    <div class="navigate__cart">
+      <i class="fas fa-shopping-cart fa-2x"></i>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'TopNavigate',
+
+  setup() {
+    const menuItems = ['electronics', 'jewelery', "men's clothing", "women's clothing"];
+    const isClicked = ref(false);
+
+    return {
+      menuItems,
+      isClicked,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../style/TopNavigate.scss";
+</style>
